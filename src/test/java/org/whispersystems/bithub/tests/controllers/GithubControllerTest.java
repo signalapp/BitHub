3fa87f9f553c53ead30089b80d117eb17f00d55b
Brandon Fish <brandon.j.fish@gmail.com>
2013-12-16 16:03:07 -0600

    Adding test with multiple authors and verifying payment amounts

--- src/test/java/org/whispersystems/bithub/tests/controllers/GithubControllerTest.java
+++ src/test/java/org/whispersystems/bithub/tests/controllers/GithubControllerTest.java
@@ -35,9 +35,7 @@ import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class GithubControllerTest extends ResourceTest {
 
@@ -98,6 +96,20 @@ public class GithubControllerTest extends ResourceTest {
     verify(coinbaseClient).sendPayment(any(Author.class),
                                        eq(BALANCE.multiply(new BigDecimal(0.02))),
                                        anyString());
+  }
+
+  @Test
+  public void testValidMultipleCommitsMultipleAuthors() throws Exception, TransferFailedException {
+    String payloadValue = "{\"after\":\"1481a2de7b2a7d02428ad93446ab166be7793fbb\",\"before\":\"17c497ccc7cca9c2f735aa07e9e3813060ce9a6a\",\"commits\":[{\"added\":[],\"author\":{\"email\":\"otherauthor@noway.biz\",\"name\":\"Garen Torikian\",\"username\":\"octokitty\"},\"committer\":{\"email\":\"lolwut@noway.biz\",\"name\":\"Garen Torikian\",\"username\":\"octokitty\"},\"distinct\":true,\"id\":\"c441029cf673f84c8b7db52d0a5944ee5c52ff89\",\"message\":\"Test\",\"modified\":[\"README.md\"],\"removed\":[],\"timestamp\":\"2013-02-22T13:50:07-08:00\",\"url\":\"https://github.com/octokitty/testing/commit/c441029cf673f84c8b7db52d0a5944ee5c52ff89\"},{\"added\":[],\"author\":{\"email\":\"lolwut@noway.biz\",\"name\":\"Garen Torikian\",\"username\":\"octokitty\"},\"committer\":{\"email\":\"lolwut@noway.biz\",\"name\":\"Garen Torikian\",\"username\":\"octokitty\"},\"distinct\":true,\"id\":\"36c5f2243ed24de58284a96f2a643bed8c028658\",\"message\":\"This is me testing the windows client.\",\"modified\":[\"README.md\"],\"removed\":[],\"timestamp\":\"2013-02-22T14:07:13-08:00\",\"url\":\"https://github.com/octokitty/testing/commit/36c5f2243ed24de58284a96f2a643bed8c028658\"},{\"added\":[\"words/madame-bovary.txt\"],\"author\":{\"email\":\"lolwut@noway.biz\",\"name\":\"Garen Torikian\",\"username\":\"octokitty\"},\"committer\":{\"email\":\"lolwut@noway.biz\",\"name\":\"Garen Torikian\",\"username\":\"octokitty\"},\"distinct\":true,\"id\":\"1481a2de7b2a7d02428ad93446ab166be7793fbb\",\"message\":\"Rename madame-bovary.txt to words/madame-bovary.txt\",\"modified\":[],\"removed\":[\"madame-bovary.txt\"],\"timestamp\":\"2013-03-12T08:14:29-07:00\",\"url\":\"https://github.com/octokitty/testing/commit/1481a2de7b2a7d02428ad93446ab166be7793fbb\"}],\"compare\":\"https://github.com/octokitty/testing/compare/17c497ccc7cc...1481a2de7b2a\",\"created\":false,\"deleted\":false,\"forced\":false,\"head_commit\":{\"added\":[\"words/madame-bovary.txt\"],\"author\":{\"email\":\"lolwut@noway.biz\",\"name\":\"Garen Torikian\",\"username\":\"octokitty\"},\"committer\":{\"email\":\"lolwut@noway.biz\",\"name\":\"Garen Torikian\",\"username\":\"octokitty\"},\"distinct\":true,\"id\":\"1481a2de7b2a7d02428ad93446ab166be7793fbb\",\"message\":\"Rename madame-bovary.txt to words/madame-bovary.txt\",\"modified\":[],\"removed\":[\"madame-bovary.txt\"],\"timestamp\":\"2013-03-12T08:14:29-07:00\",\"url\":\"https://github.com/octokitty/testing/commit/1481a2de7b2a7d02428ad93446ab166be7793fbb\"},\"pusher\":{\"email\":\"lolwut@noway.biz\",\"name\":\"Garen Torikian\"},\"ref\":\"refs/heads/master\",\"repository\":{\"created_at\":1332977768,\"description\":\"\",\"fork\":false,\"forks\":0,\"has_downloads\":true,\"has_issues\":true,\"has_wiki\":true,\"homepage\":\"\",\"id\":3860742,\"language\":\"Ruby\",\"master_branch\":\"master\",\"name\":\"testing\",\"open_issues\":2,\"owner\":{\"email\":\"lolwut@noway.biz\",\"name\":\"octokitty\"},\"private\":false,\"pushed_at\":1363295520,\"size\":2156,\"stargazers\":1,\"url\":\"https://github.com/moxie0/test\",\"watchers\":1}}";
+    MultivaluedMapImpl post = new MultivaluedMapImpl();
+    post.add("payload", payloadValue);
+    ClientResponse response = client().resource("/v1/github/commits/").header("X-Forwarded-For", "192.30.252.1")
+        .type(MediaType.APPLICATION_FORM_URLENCODED_TYPE).post(ClientResponse.class, post);
+
+    verify(coinbaseClient, times(1)).sendPayment(any(Author.class), eq(BALANCE.multiply(new BigDecimal(0.02))),
+        anyString());
+    verify(coinbaseClient, times(1)).sendPayment(any(Author.class), eq(BALANCE.subtract(BALANCE.multiply(new BigDecimal(0.02)))
+        .multiply(new BigDecimal(0.02))), anyString());
   }
 
 }
